fe299c9c0ec8ae82cbe90744ef1d6bdb0a5ab565
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-09-19 11:47:24 -0300

    Criação dos métodos da service e ajuste no contrato

--- TC_Clinica_Gerenciamento/Contracts/IAgendaService.cs
+++ TC_Clinica_Gerenciamento/Contracts/IAgendaService.cs
@@ -6,13 +6,13 @@ using TCC_Unip.Models.Servico;
 namespace TCC_Unip.Contracts
 {
     interface IAgendaService
-    {        
-        ResultService<List<Agenda>> List();
+    {
+        ResultService<Agenda> Get(string id);
+        ResultService<List<Agenda>> ListAgendasPeriodo(DateTime dateFrom, DateTime dateTo);
+        ResultService<Paciente> ConsultasPeriodoPaciente(string cpf, DateTime dateFrom, DateTime dateTo);
+        ResultService<Funcionario> ConsultasPeriodoFuncionario(string cpf, DateTime dateFrom, DateTime dateTo);
         ResultService<bool> Save(Agenda model);
-        ResultService<bool> Delete(int id);
-        ResultService<List<Agenda>> GetAgenda(DateTime inicio, DateTime fim);
-        ResultService<Paciente> GetAgendaPaciente(DateTime inicio, DateTime fim);
-        ResultService<Funcionario> GetAgendaFuncionario(DateTime inicio, DateTime fim);
+        ResultService<bool> Delete(string id);
 
     }
 }
--- TC_Clinica_Gerenciamento/Services/AgendaService.cs
+++ TC_Clinica_Gerenciamento/Services/AgendaService.cs
@@ -1,8 +1,99 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using TCC_Unip.Models.Local;
+using TCC_Unip.Models.Servico;
+using TCC_Unip.ServiceAPI;
 
 namespace TCC_Unip.Services
 {
-    public class AgendaService
+    public class AgendaService : Contracts.IAgendaService
     {
+        AgendaServiceAPI service = new AgendaServiceAPI();
+
+        public ResultService<Agenda> Get(string id)
+        {
+            var result = new ResultService<Agenda>();
+
+            var retorno = service.Get(id);
+            result.value = retorno;
+
+            if (string.IsNullOrEmpty(result.value.Modalidade))
+                result.errorMessage = "A Consulta não existe mais na base de dados do serviço!";
+
+            return result;
+        }
+
+        public ResultService<List<Agenda>> ListAgendasPeriodo(DateTime dateFrom, DateTime dateTo)
+        {
+            var result = new ResultService<List<Agenda>>();
+
+            var retorno = service.ListAgendasPeriodo(dateFrom.Date.ToString(), dateTo.Date.ToString());
+            result.value = retorno;
+
+            return result;
+        }
+
+        public ResultService<Funcionario> ConsultasPeriodoFuncionario(string cpf, DateTime dateFrom, DateTime dateTo)
+        {
+            var result = new ResultService<Funcionario>();
+
+            var retorno = service.ConsultasPeriodoFuncionario(cpf, dateFrom.Date.ToString(), dateTo.Date.ToString());
+            result.value = retorno;
+
+            return result;
+        }
+
+        public ResultService<Paciente> ConsultasPeriodoPaciente(string cpf, DateTime dateFrom, DateTime dateTo)
+        {
+            var result = new ResultService<Paciente>();
+
+            var retorno = service.ConsultasPeriodoPaciente(cpf, dateFrom.Date.ToString(), dateTo.Date.ToString());
+            result.value = retorno;
+
+            return result;
+        }
+
+        public ResultService<bool> Save(Agenda model)
+        {
+            var result = new ResultService<bool>();
+
+            if (model.Id == 0)
+            {
+                var retorno = service.Save(model);
+                result.value = retorno;
+
+                if (result.value)
+                    result.message = "Consulta salva com sucesso!";
+                else
+                    result.errorMessage = "Falha ao salvar a Consulta!";
+            }
+            else
+            {
+                var retorno = service.Update(model);
+                result.value = retorno;
+
+                if (result.value)
+                    result.message = "Consulta atualizada com sucesso!";
+                else
+                    result.errorMessage = "Falha ao atualizar a Consulta!";
+            }
+
+            return result;
+        }
+
+        public ResultService<bool> Delete(string id)
+        {
+            var result = new ResultService<bool>();
+
+            var retorno = service.Delete(id);
+            result.value = retorno;
+
+            if (result.value)
+                result.message = "Consulta excluída com sucesso!";
+            else
+                result.errorMessage = "Falha ao excluir a Consulta!";
+
+            return result;
+        }
     }
 }
