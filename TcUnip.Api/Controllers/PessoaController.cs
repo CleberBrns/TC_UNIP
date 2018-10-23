64a6fa021ce7321c4fc14a50cb534e63370c60f4
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-10-23 13:24:54 -0300

    Inclusão dos métodos da service do Funcionario e Usuario

--- TcUnip.Api/Controllers/PessoaController.cs
+++ TcUnip.Api/Controllers/PessoaController.cs
@@ -11,7 +11,9 @@ namespace TcUnip.Api.Controllers
         readonly IFuncionarioService _funcionarioService;
         readonly IUsuarioService _usuarioService;        
 
-        public PessoaController(IPacienteService pacienteService, IFuncionarioService funcionarioService, IUsuarioService usuarioService)
+        public PessoaController(IPacienteService pacienteService, 
+                                IFuncionarioService funcionarioService, 
+                                IUsuarioService usuarioService)
         {
             this._pacienteService = pacienteService;
             this._funcionarioService = funcionarioService;
@@ -68,9 +70,120 @@ namespace TcUnip.Api.Controllers
 
         #region Funcionário
 
+        [HttpGet]
+        [Route("api/Pessoa/GetFuncionario/{cpf}")]
+        public IHttpActionResult GetFuncionario(string cpf)
+        {
+            var retorno = _funcionarioService.Get(cpf);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpGet]
+        [Route("api/Pessoa/ListFuncionarios/{getFromSession}")]
+        public IHttpActionResult ListFuncionarios(bool getFromSession)
+        {
+            var retorno = _funcionarioService.List(getFromSession);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpGet]
+        [Route("api/Pessoa/ListProfissionais/{getFromSession}")]
+        public IHttpActionResult ListProfissionais(bool getFromSession)
+        {
+            var retorno = _funcionarioService.ListProfissionais(getFromSession);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpPost]
+        [Route("api/Pessoa/SalvaFuncionario")]
+        public IHttpActionResult SalvaFuncionario(Funcionario model)
+        {
+            var retorno = _funcionarioService.Save(model);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpDelete]
+        [Route("api/Pessao/ExcluiFuncionario")]
+        public IHttpActionResult ExcluiFuncionario(string cpf)
+        {
+            var retorno = _funcionarioService.Delete(cpf);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
         #endregion
 
         #region Usuário
+
+        [HttpGet]
+        [Route("api/Pessoa/GetUsuario/{cpf}")]
+        public IHttpActionResult GetUsuario(string email)
+        {
+            var retorno = _usuarioService.Get(email);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpGet]
+        [Route("api/Pessoa/ListUsuarios/{getFromSession}")]
+        public IHttpActionResult ListUsuarios(bool getFromSession)
+        {
+            var retorno = _usuarioService.List(getFromSession);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpPost]
+        [Route("api/Pessoa/AutenticaUsuario")]
+        public IHttpActionResult AutenticaUsuario(Usuario model)
+        {
+            var retorno = _usuarioService.Auth(model);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpPost]
+        [Route("api/Pessoa/SalvaUsuario")]
+        public IHttpActionResult SalvaUsuario(Usuario model)
+        {
+            var retorno = _usuarioService.Save(model);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
+        [HttpDelete]
+        [Route("api/Pessao/ExcluiUsuario")]
+        public IHttpActionResult ExcluiUsuario(string cpf)
+        {
+            var retorno = _usuarioService.Delete(cpf);
+            if (!retorno.Status)
+                return new InvalidListMessageResult(retorno.Message);
+
+            return Ok(retorno);
+        }
+
 
         #endregion
     }
--- TcUnip.Service.Contract/Pessoa/IFuncionarioService.cs
+++ TcUnip.Service.Contract/Pessoa/IFuncionarioService.cs
@@ -8,6 +8,7 @@ namespace TcUnip.Service.Contract.Pessoa
     {
         Result<Funcionario> Get(string cpf);
         Result<List<Funcionario>> List(bool getFromSession);
+        Result<List<Funcionario>> ListProfissionais(bool getFromSession);
         Result<bool> Save(Funcionario model);
         Result<bool> Delete(string cpf);
     }
