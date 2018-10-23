d3446251efa96dcc3e8d244b28bbe4ede40232cc
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-10-23 08:50:27 -0300

    Ajustes dos Contratos de Session e ServiceAPI

---
+++ TcUnip.Service.Contract/ServiceApi/IServiceAPIBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TcUnip.Service.Contract.ServiceApi
+{
+    public interface IServiceAPIBase<TModel>
+    {
+        TModel Get(string id);
+        List<TModel> List();
+        bool Save(TModel model);
+        bool Update(TModel model);
+        bool Delete(string id);
+    }
+}
--- TcUnip.Session/Pessoa/UsuarioSN.cs
+++ TcUnip.Session/Pessoa/UsuarioSN.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TcUnip.Model.Pessoa;
 using TcUnip.Session.Common;
 using TcUnip.Session.Contract;
