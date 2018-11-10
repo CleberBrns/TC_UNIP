be0c5154953df4b2a48caaacba98c4809b112ec2
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-10 17:31:48 -0200

    Ajuste Seed Para inclusão de dados iniciais

--- TcUnip.Data.Entity/SeedItens/TipoPerfilSeed.cs
+++ TcUnip.Data.Entity/SeedItens/TipoPerfilSeed.cs
@@ -10,40 +10,36 @@ namespace TcUnip.Data.Entity.SeedItens
 {
     public class TipoPerfilSeed
     {
-        public static List<TipoPerfil> Seed(TcUnipContext context)
+        public static void Seed(TcUnipContext context)
         {
             var listBD = context.TipoPerfil.ToList();
-            if (listBD.Any())
-                return listBD.ToList();
-
-            var arraySalvar = new TipoPerfil[]
+            if (!listBD.Any())
             {
+                var arraySalvar = new TipoPerfil[]
+                {
                     new TipoPerfil {Id = 1, Tipo = "Administrador", Permissao = "ADMINISTRACAO"},
                     new TipoPerfil {Id = 2, Tipo = "Recepcionista", Permissao = "RECEPCAO"},
                     new TipoPerfil {Id = 3, Tipo = "Profissional", Permissao = "PROFISSIONAL"}
-            };
+                };
 
-            var listSalvar = new List<TipoPerfil>();
+                var listSalvar = new List<TipoPerfil>();
 
-            foreach (var item in arraySalvar)
-            {
-                if (listBD.Find(c => c.Id == item.Id && 
-                                     c.Tipo == item.Tipo && 
-                                     c.Permissao == item.Permissao) == null)
+                foreach (var item in arraySalvar)
                 {
-                    listSalvar.Add(item);
+                    if (listBD.Find(c => c.Id == item.Id &&
+                                         c.Tipo == item.Tipo &&
+                                         c.Permissao == item.Permissao) == null)
+                    {
+                        listSalvar.Add(item);
+                    }
                 }
-            }
 
-            if (listSalvar.Count > 0)
-            {
-                context.TipoPerfil.AddOrUpdate(listSalvar.ToArray());
-                context.SaveChanges();
-
-                listSalvar = context.TipoPerfil.ToList();
-            }
-
-            return context.TipoPerfil.ToList();
+                if (listSalvar.Count > 0)
+                {
+                    context.TipoPerfil.AddOrUpdate(listSalvar.ToArray());
+                    context.SaveChanges();
+                }
+            }          
         }
     }
 }
