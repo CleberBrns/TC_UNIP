ce60c582a90b882bc5c14b56daa1de11b628031a
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-10 20:28:16 -0200

    Inclusão dos contratos das Service

--- TcUnip.Model/Agenda/PesquisaModel.cs
+++ TcUnip.Model/Agenda/PesquisaModel.cs
@@ -8,6 +8,7 @@ namespace TcUnip.Model.Agenda
 {
     public class PesquisaModel
     {
+        public int IdCadastro { get; set; }
         public string CpfCadastro { get; set; }
         public DateTime DataIncio { get; set; }
         public DateTime DataFim { get; set; }        
