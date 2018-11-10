b94310cf228414f5dd5afa3d9a7634bfeb34fab2
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-10 17:59:01 -0200

    Inclusão do projeto de configuração padrão as demais classes do projeto

---
+++ TcUnip.Config/ConfigureApiContainer.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Unity;
+
+namespace TcUnip.Config
+{
+    public class ConfigureApiContainer
+    {
+        public static void InitializeContainer(IUnityContainer container, bool fake = false)
+        {
+
+        }
+    }
+}
