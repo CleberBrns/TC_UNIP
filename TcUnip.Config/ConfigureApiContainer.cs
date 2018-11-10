573344f6a5f8e412d2b981fdbf4b9123daeb910c
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-10 20:02:12 -0200

    Mapeamento dos objetos Model para os objetos do Banco de Dados

--- TcUnip.Config/ConfigureApiContainer.cs
+++ TcUnip.Config/ConfigureApiContainer.cs
@@ -1,4 +1,10 @@
 ﻿using AutoMapper;
+using TcUnip.Data.Entity.Modelagem.Agenda;
+using TcUnip.Data.Entity.Modelagem.Cadastro;
+using TcUnip.Data.Entity.Modelagem.FluxoCaixa;
+using TcUnip.Model.Agenda;
+using TcUnip.Model.Cadastro;
+using TcUnip.Model.FluxoCaixa;
 using Unity;
 
 namespace TcUnip.Config
@@ -7,7 +13,37 @@ namespace TcUnip.Config
     {
         public static void InitializeContainer(IUnityContainer container, bool fake = false)
         {
+            container.RegisterInstance<IMapper>(InitializeAutoMapper().CreateMapper());
+        }
 
+        private static MapperConfiguration InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                /*O 'ReverseMap()' cria apenas um mapeamento inverso simples.
+                  Caso o mapeamento tenha alguma configuração especifica, 
+                  devemos criar manualmente os dois formato do map*/
+
+                #region Model to Entity
+
+                //Cadastro
+                cfg.CreateMap<FuncionarioModel, Funcionario>().ReverseMap();
+                cfg.CreateMap<PacienteModel, Paciente>().ReverseMap();
+                cfg.CreateMap<PessoaModel, Pessoa>().ReverseMap();
+                cfg.CreateMap<TipoPerfilModel, TipoPerfil>().ReverseMap();
+                cfg.CreateMap<UsuarioModel, Usuario>().ReverseMap();
+
+                //Agenda
+                cfg.CreateMap<ModalidadeModel, Modalidade>().ReverseMap();
+                cfg.CreateMap<SessaoModel, Sessao>().ReverseMap();
+                cfg.CreateMap<StatusSessaoModel, StatusSessao>().ReverseMap();
+
+                //Fluxo Caixa
+                cfg.CreateMap<CaixaModel, Caixa>().ReverseMap();
+
+                #endregion
+            });
+            return config;
         }
     }
 }
