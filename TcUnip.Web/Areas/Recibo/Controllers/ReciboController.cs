400e0751dfa29229326378e94afe2d56bc700bd6
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-11 16:14:12 -0200

    Implementação da listagem e visualização dos recibos

---
+++ TcUnip.Web/Areas/Recibo/Controllers/ReciboController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TcUnip.Model.Common;
+using TcUnip.Web.Controllers;
+using TcUnip.Web.Models.Proxy.Contract;
+using TcUnip.Web.Util;
+
+namespace TcUnip.Web.Areas.Recibo.Controllers
+{
+    public class ReciboController : BaseController
+    {
+        readonly IReciboProxy _reciboProxy;
+        readonly Mensagens mensagens = new Mensagens();
+        readonly ReplacesService replacesService = new ReplacesService();
+
+        public ReciboController(IReciboProxy reciboProxy)
+        {
+            this._reciboProxy = reciboProxy;
+        }
+
+        public ActionResult Index()
+        {
+            var userInfo = GetUsuarioSession();
+            if (!userInfo.Item2)
+                return RedirectToAction("Login", "Login", new { area = "" });
+
+            ViewBag.Usuario = userInfo.Item1;
+
+            return PartialView("_Index");
+        }
+
+        public ActionResult ListaRecibosDoDia()
+        {
+            ViewBag.Usuario = GetUsuarioSession().Item1;
+
+            string msgExibicao = string.Empty;
+            string msgAnalise = string.Empty;
+
+            try
+            {
+                var resultService = _reciboProxy.ListRecibosDoDia();
+
+                var list = FiltraListaPorPerfil(resultService.Value);
+
+                msgExibicao = resultService.Message;
+                msgAnalise = !resultService.Status ? "Falha!" : string.Empty;
+
+                return PartialView("_GridRecibos", list);
+            }
+            catch (Exception ex)
+            {
+                msgExibicao = Constants.Constants.msgFalhaAoListar;
+                msgAnalise = ex.ToString();
+            }
+
+            var mensagensRetorno = mensagens.ConfiguraMensagemRetorno(msgExibicao, msgAnalise);
+            return Json(new { mensagensRetorno }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ListaRecibosPorDatas(string dataInicio, string dataFim)
+        {
+            ViewBag.Usuario = GetUsuarioSession().Item1;
+
+            string msgExibicao = string.Empty;
+            string msgAnalise = string.Empty;
+
+            try
+            {
+                var resultService = _reciboProxy.ListRecibosPeriodo(dataInicio, dataFim);
+
+                var list = FiltraListaPorPerfil(resultService.Value);
+
+                msgExibicao = resultService.Message;
+                msgAnalise = !resultService.Status ? "Falha!" : string.Empty;
+
+                return PartialView("_GridRecibos", list);
+            }
+            catch (Exception ex)
+            {
+                msgExibicao = Constants.Constants.msgFalhaAoListar;
+                msgAnalise = ex.ToString();
+            }
+
+            var mensagensRetorno = mensagens.ConfiguraMensagemRetorno(msgExibicao, msgAnalise);
+            return Json(new { mensagensRetorno }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ModalVisualizar(string id)
+        {
+            ValidaAutorizaoAcessoUsuario(Constants.ConstPermissoes.gerenciamento);
+
+            string msgExibicao = string.Empty;
+            string msgAnalise = string.Empty;
+
+            ViewBag.Usuario = GetUsuarioSession().Item1;
+
+            try
+            {
+                var resultService = _reciboProxy.Get(id);
+
+                if (resultService.Status)
+                {
+                    return PartialView("_Visualizar", resultService.Value);
+                }
+                else
+                {
+                    msgExibicao = resultService.Message;
+                    msgAnalise = "Erro!";
+                }
+            }
+            catch (Exception ex)
+            {
+                msgExibicao = Constants.Constants.msgFalhaAoCarregar;
+                msgAnalise = ex.Message;
+            }
+
+            var mensagensRetorno = mensagens.ConfiguraMensagemRetorno(msgExibicao, msgAnalise);
+            return Json(new { mensagensRetorno }, JsonRequestBehavior.AllowGet);
+        }
+
+        #region Métodos Privados
+
+        private List<Model.Contabil.Recibo> FiltraListaPorPerfil(List<Model.Contabil.Recibo> listRecibos)
+        {
+            if (listRecibos.Count > 0 &&
+                Constants.ConstPermissoes.profissional.Equals(GetUsuarioSession().Item1.Permissoes.FirstOrDefault()))
+            {
+                listRecibos = 
+                    listRecibos.Where(l => l.Profissional.Equals(GetUsuarioSession().Item1.Funcionario.Nome))
+                               .ToList();
+            }
+
+            return listRecibos;
+        }
+
+        #endregion
+
+    }
+}
---
+++ TcUnip.Web/Areas/Recibo/ReciboAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TcUnip.Web.Areas.Recibo
+{
+    public class ReciboAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Recibo";
+            }
+        }  
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Recibo_default",
+                "Recibo/{controller}/{action}/{id}",
+                new { Area = "Recibo", Controller = "Recibo", action = "Index", id = UrlParameter.Optional },
+                new[] { "TcUnip.Web.Areas.Recibo.Controllers" }
+            );
+        }
+    }
+}
