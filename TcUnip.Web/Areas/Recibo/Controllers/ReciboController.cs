41b65cd17f0253eef1a9b05388b8f51cb3da4941
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-11 17:47:01 -0200

    Finalização da visualização do recibo e impressão

--- TcUnip.Web/Areas/Recibo/Controllers/ReciboController.cs
+++ TcUnip.Web/Areas/Recibo/Controllers/ReciboController.cs
@@ -11,13 +11,15 @@ namespace TcUnip.Web.Areas.Recibo.Controllers
 {
     public class ReciboController : BaseController
     {
+        readonly IAgendaProxy _agendaProxy;
         readonly IReciboProxy _reciboProxy;
         readonly Mensagens mensagens = new Mensagens();
         readonly ReplacesService replacesService = new ReplacesService();
 
-        public ReciboController(IReciboProxy reciboProxy)
+        public ReciboController(IReciboProxy reciboProxy, IAgendaProxy agendaProxy)
         {
             this._reciboProxy = reciboProxy;
+            this._agendaProxy = agendaProxy;
         }
 
         public ActionResult Index()
@@ -103,6 +105,45 @@ namespace TcUnip.Web.Areas.Recibo.Controllers
                 if (resultService.Status)
                 {
                     return PartialView("_Visualizar", resultService.Value);
+                }
+                else
+                {
+                    msgExibicao = resultService.Message;
+                    msgAnalise = "Erro!";
+                }
+            }
+            catch (Exception ex)
+            {
+                msgExibicao = Constants.Constants.msgFalhaAoCarregar;
+                msgAnalise = ex.Message;
+            }
+
+            var mensagensRetorno = mensagens.ConfiguraMensagemRetorno(msgExibicao, msgAnalise);
+            return Json(new { mensagensRetorno }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ModalVisualizarImpressao(string id)
+        {
+            ValidaAutorizaoAcessoUsuario(Constants.ConstPermissoes.gerenciamento);
+
+            string msgExibicao = string.Empty;
+            string msgAnalise = string.Empty;
+
+            ViewBag.Usuario = GetUsuarioSession().Item1;
+
+            try
+            {
+                var resultService = _agendaProxy.Get(id);                
+
+                if (resultService.Status)
+                {
+                    var constants = new Constants.Constants();
+
+                    ViewBag.Modalidade = 
+                        constants.ListModalidades().Where(m => m.Value.Equals(resultService.Value.Modalidade))
+                        .Select(m => m.Name).FirstOrDefault();
+
+                    return PartialView("_Impressao", resultService.Value);
                 }
                 else
                 {
---
+++ TcUnip.Web/Scripts/Default/Impressao.js
@@ -0,0 +1,41 @@
+﻿
+
+var configuraImpressao = {
+
+    Imprimir: function (selector) {
+        setTimeout(function () {
+            configuraImpressao.Popup(document.getElementById(selector).innerHTML);
+        }, 500);
+    },
+    Popup: function (data) {
+
+        var mywindow = window.open('');
+        mywindow.document.write('<html><head><title>Imprimir</title>');
+        mywindow.document.write(configuraImpressao.StyleSheetsPage());
+        mywindow.document.write('</head><body >');
+        mywindow.document.write(data);
+        mywindow.document.write('</body></html>');
+
+        mywindow.document.close();
+        mywindow.focus();
+
+        mywindow.document.body.onload = function () {
+            // continue to print
+            mywindow.print();
+            mywindow.close();
+        };
+
+
+        return true;
+    },
+    StyleSheetsPage: function () {
+        var stylesPage = "";
+        for (var i = 0; i < document.styleSheets.length; i++) {
+
+            stylesPage += '<link rel="stylesheet" href="' + document.styleSheets[i].href + '" type="text/css" />'
+
+        }
+        return stylesPage;
+    }
+
+};
