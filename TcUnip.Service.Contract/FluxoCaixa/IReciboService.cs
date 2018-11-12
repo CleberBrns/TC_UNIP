e64a36c1540c5b54b42632f5e9a20a9e76cd6391
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-12 11:07:44 -0200

    Inclusão do contrato de servico para o Rebico

---
+++ TcUnip.Service.Contract/FluxoCaixa/IReciboService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TcUnip.Model.Common;
+using TcUnip.Model.FluxoCaixa;
+
+namespace TcUnip.Service.Contract.FluxoCaixa
+{
+    public interface IReciboService
+    {
+        Result<ReciboModel> Get(string id);
+        Result<List<ReciboModel>> ListRecibosPeriodo(string dateFrom, string dateTo);
+        Result<List<ReciboModel>> ListRecibosDoDia();
+        Result<List<ReciboModel>> ListRecibosPeriodoPaciente(string cpf, string dateFrom, string dateTo);
+        Result<List<ReciboModel>> ListRecibosPeriodoFuncionario(string cpf, string dateFrom, string dateTo);
+    }
+}
