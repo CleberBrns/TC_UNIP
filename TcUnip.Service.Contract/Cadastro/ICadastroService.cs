3d616fcd596190952dce5a3427db94b303b7a82b
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-11-12 11:44:52 -0200

    Inclusão dos contratos de Cadastro e Fluxo de caixão, onde estarão concentrados os seus especifícos acessos as entidades contempladas dos respectivos tipos

---
+++ TcUnip.Service.Contract/Cadastro/ICadastroService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcUnip.Model.Cadastro;
+using TcUnip.Model.Common;
+
+namespace TcUnip.Service.Contract.Cadastro
+{
+    public interface ICadastroService
+    {
+        #region Usuario
+
+        Result<UsuarioModel> GetUsuario(int id);
+        Result<List<UsuarioModel>> ListUsuario();
+        Result<bool> SalvaUsuario(UsuarioModel model);
+        Result<bool> ExcliuUsuario(int id);
+        Result<UsuarioModel> AutenticaUsuario(UsuarioModel model);
+
+        #endregion
+
+        #region Paciente
+
+        Result<PacienteModel> GetPaciente(int id);
+        Result<List<PacienteModel>> ListPaciente();
+        Result<bool> SalvaPaciente(PacienteModel model);
+        Result<bool> ExcluiPaciente(int id);
+
+        #endregion
+
+        #region Funcionario
+
+        Result<FuncionarioModel> GetFuncionario(int id);
+        Result<List<FuncionarioModel>> ListFuncionario();
+        Result<List<FuncionarioModel>> ListProfissionais();
+        Result<bool> SalvaFuncionario(FuncionarioModel model);
+        Result<bool> ExcluiFuncionario(int id);
+
+        #endregion
+    }
+}
---
+++ TcUnip.Service.Contract/FluxoCaixa/IFluxoCaixa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcUnip.Model.Agenda;
+using TcUnip.Model.Common;
+using TcUnip.Model.FluxoCaixa;
+
+namespace TcUnip.Service.Contract.FluxoCaixa
+{
+    public interface IFluxoCaixa
+    {
+        #region Caixa
+
+        Result<CaixaModel> GetCaixa(int id);
+        Result<List<CaixaModel>> ListCaixaPeriodo(PesquisaModel pesquisaModel);
+        Result<CaixaModel> ListCaixaPeriodoPaciente(PesquisaModel pesquisaModel);
+        Result<CaixaModel> ListCaixaPeriodoFuncionario(PesquisaModel pesquisaModel);
+
+        #endregion
+
+        #region Recibo
+
+        Result<ReciboModel> GetRecibo(string id);
+        Result<List<ReciboModel>> ListRecibosPeriodo(PesquisaModel pesquisaModel);
+        Result<List<ReciboModel>> ListRecibosDoDia();
+        Result<List<ReciboModel>> ListRecibosPeriodoPaciente(PesquisaModel pesquisaModel);
+        Result<List<ReciboModel>> ListRecibosPeriodoFuncionario(PesquisaModel pesquisaModel);
+
+        #endregion
+    }
+}
